f58105e23e76b76be8f16eaf7516b73c34ec1769
USUARIO <brortiza@unal.edu.co>
2025-09-04 11:18:36 -0500

    Poo

---
+++ Primer.py
@@ -0,0 +1,24 @@
+# Bryan Ortiz
+# Primera version
+
+# 1. Estructuras de control
+# if, for, while, switch, dowhile, 
+
+# Prueba
+# Estructura un programa que mencione si su calificación es meritoria
+
+Nota = float(input("Escriba su nota:    "))
+
+if (Nota >= 4.5 ):
+    print("Esta nota es meritoria")
+else:
+    print("Esta nota no es meritoria")
+
+# Modulo %, AND, OR
+
+val = 10
+
+if(val%2):
+    print(f"{val} es un número par")
+else:
+    print(f"{val} no es un número par")
